869aced04ab312cdb6c4d660b0efc685227b2bc5
dozken <dosmukhamed@gmail.com>
2018-10-26 19:39:36 +0600

    init

--- src/leetcode/easy/JewelsAndStones771.java
+++ src/leetcode/easy/JewelsAndStones771.java
@@ -1,4 +1,4 @@
-package easy;
+package leetcode.easy;
 
 public class JewelsAndStones771 {
     public static void main(String args[]){
--- src/leetcode/easy/ReverseInteger7.java
+++ src/leetcode/easy/ReverseInteger7.java
@@ -1,4 +1,4 @@
-package easy;
+package leetcode.easy;
 
 public class ReverseInteger7 {
     public static void main(String args[]) {
