e3b725b2c05aeff75d961c2849ece37539f3c568
dosmukhamed <dosmukhamed@gmail.com>
2019-01-04 05:55:00 +0600

    922

---
+++ src/main/java/leetcode/easy/SortArrayByParityII922.java
@@ -0,0 +1,20 @@
+package leetcode.easy;
+
+public class SortArrayByParityII922 {
+    public int[] sortArrayByParityII(int[] A) {
+        int length = A.length;
+        int[] result = new int[length];
+        int evenIndex = 0;
+        int oddIndex = 1;
+        for (int number : A) {
+            if (number % 2 == 0) {
+                result[evenIndex] = number;
+                evenIndex += 2;
+            } else {
+                result[oddIndex] = number;
+                oddIndex += 2;
+            }
+        }
+        return result;
+    }
+}
