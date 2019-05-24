ac353e89faa2117d3310d2aa6c3294dedc126236
dozken <dosmukhamed@gmail.com>
2019-05-24 11:01:33 +0600

    more leetcode

---
+++ src/main/java/leetcode/easy/IntersectionofTwoLinkedLists160.java
@@ -0,0 +1,42 @@
+package leetcode.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionofTwoLinkedLists160 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return b;
+
+    }
+
+    public ListNode getIntersectionNodeB(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+
+        return null;
+    }
+}
